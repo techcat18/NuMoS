8c17779aca479130358d5eeeea086bf3d1646d3d
Kostiantyn Bondarenko <bondarenko@managed-code.com>
2023-11-12 11:41:11 +0200

    added pictures

--- startup-atom/src/app/app.component.ts
+++ startup-atom/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SlideInterface } from './image-slider/image-slider/image-slider';
 
 @Component({
@@ -6,11 +6,28 @@ import { SlideInterface } from './image-slider/image-slider/image-slider';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'startup-atom';
   slides : SlideInterface[] = [
     { url: 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg', title: 'beach' },
     { url: 'https://image.petmd.com/files/styles/978x550/public/2023-09/how-smart-are-cats.jpg', title: 'boat' },
     { url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/1200px-RedCat_8727.jpg', title: 'forest' }
   ];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.fadeOutAtom();
+    }, 1500);
+  }
+
+  fadeOutAtom() {
+    const atomElement = document.querySelector('.atom') as HTMLElement;
+    if (atomElement) {
+      atomElement.style.transition = 'opacity 1.5s';
+      atomElement.style.opacity = '0';
+      setTimeout(() => {
+        atomElement.remove();
+      }, 1500);
+    }
+  }
 }
